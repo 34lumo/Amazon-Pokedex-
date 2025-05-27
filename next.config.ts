d44e8abf6fa4aa1eb0615b728a52bf6cbcb33ce1
TheBigDeal <161509876+34lumo@users.noreply.github.com>
2025-05-27 15:08:35 +0200

    updated configuration and proxy rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/pokemon',
+        destination: 'https://<TU_BACKEND_URL>/Prod/pokemon',
+      },
+      {
+        source: '/api/pokemon/:id',
+        destination: 'https://<TU_BACKEND_URL>/Prod/pokemon/:id',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
